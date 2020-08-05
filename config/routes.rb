69635af08d66d4a49ec956be0dab984106ee0bea
kumapaaan <kumapaaan@users.noreply.github.com>
2020-08-05 15:28:32 +0900

    add scss front

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'premiams#index'
   resources :premiams
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
